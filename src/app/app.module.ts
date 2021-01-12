86dab03a1f33352841d667834b90b7675c3c1a33
samridhg <samridhg@thoughtworks.com>
2021-01-12 13:37:10 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from '../product/product.component';
+import { StoreModule } from '@ngrx/store';
+import {addProductReducer} from "../reducers/product.reducer";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({product: addProductReducer})
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/product/product.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {Product} from "./product.model";
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  products: Observable<Product>;
+
+  constructor(private store: Store<AppState>) {
+    this.products = this.store.select(state => state.product)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
